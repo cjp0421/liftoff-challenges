5aed16352cfc58363499f583d2c733828d4dd4cb
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-18 11:03:18 -0500

    'completed week 3 code challenge'

---
+++ Week3_Challenge.js
@@ -0,0 +1,22 @@
+function LongestWord(sen) {
+
+
+    let i = 0;
+    let wordArray = sen.split(' ');
+
+    let regex = /[a-zA-Z0-9]/g
+
+    // console.log(wordArray[i].match(regex).length);
+    let longestWord = wordArray.sort(function (a, b) {
+        return b.match(regex).length - a.match(regex).length;
+    });
+
+
+    return longestWord[0];
+
+
+
+}
+
+// keep this function call here 
+console.log(LongestWord(readline()));
