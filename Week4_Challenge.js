66b9b38aee5fa51e4a64b4791ce6fbcc4ed7ee77
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-31 13:04:41 -0500

    commented out console.log

--- Week4_Challenge.js
+++ Week4_Challenge.js
@@ -2,7 +2,7 @@ function addUp(num) {
     let addedUpNumber = 0;
     for (let i = 1; i <= num; i++) {
         addedUpNumber += i;
-        console.log(addedUpNumber);
+        // console.log(addedUpNumber);
 
     }
     return addedUpNumber;
