bad99820bab628679c8e1f07015f71b55e13350e
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2023-07-31 13:03:13 -0500

    completed week4 challenge

---
+++ Week4_Challenge.js
@@ -0,0 +1,11 @@
+function addUp(num) {
+    let addedUpNumber = 0;
+    for (let i = 1; i <= num; i++) {
+        addedUpNumber += i;
+        console.log(addedUpNumber);
+
+    }
+    return addedUpNumber;
+}
+
+// addUp(4);
